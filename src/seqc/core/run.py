f9e5467bdf2445d0feacbe2998da2f193142f58c
adamgayoso <adamgayoso@users.noreply.github.com>
2019-03-15 15:19:57 -0700

    m_scape uses same insert size as 10x

--- src/seqc/core/run.py
+++ src/seqc/core/run.py
@@ -227,9 +227,9 @@ def run(args) -> None:
                        'the user upon termination of SEQC run.')
 
         max_insert_size = args.max_insert_size
-        if (args.platform == "ten_x") or (args.platform == "ten_x_v2"):
+        if (args.platform == "ten_x") or (args.platform == "ten_x_v2") or (args.platform == "m_scape"):
             max_insert_size = 10000
-            log.notify("Full length transcripts are used for read mapping in 10x data.")
+            log.notify("Full length transcripts are used for read mapping in 10x/m_scape data.")
             args.filter_low_coverage = False
 
         log.args(args)
